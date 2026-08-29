f00baebb93e62598e5368e94480a8d74097f9532
leonscott <leonscott543@gmail.com>
2026-08-28 20:40:51 -0500

    Add initial implementation of main function with runtime printouts

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+// Leon Scott 
+// Created: 8/28/2026
+
+/*
+Course of action: 
+[] general runtime prnntout title header and footer
+[] exit runtime [q]
+*/
+
+pub fn main(){
+    println!("------------------------------------------");
+    println!("---------- LET THERE BE LIGHT ------------");
+    println!("------------------------------------------\n\n");
+
+
+
+
+
+
+
+
+
+
+
+    println!("\n\n------------------------------------------");
+    println!("---------- AND            CUT ------------");
+    println!("------------------------------------------\n\n");
+
+}
