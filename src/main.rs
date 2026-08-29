0e80d59ce069093c54d12e635e1f81aa4e04e801
leonscott <leonscott543@gmail.com>
2026-08-29 08:41:16 -0500

    Update main function to implement exit processing and mark task completion

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,31 @@
 
 /*
 Course of action: 
-[] general runtime prnntout title header and footer
-[] exit runtime [q]
+[x] general runtime prnntout title header and footer
+[] exit runtime [q]... input processing
 */
+use std::io;
 
 pub fn main(){
     println!("------------------------------------------");
     println!("---------- LET THERE BE LIGHT ------------");
     println!("------------------------------------------\n\n");
 
-
-
-
-
-
-
-
-
+    
 
 
     println!("\n\n------------------------------------------");
     println!("---------- AND            CUT ------------");
     println!("------------------------------------------\n\n");
+
+    println!("[q] then ENTER to quit : ");
+    let mut exit = String::new();
+    io::stdin()
+        .read_line(&mut exit)
+        .expect("Failed to read line");
+
+    if exit == "q" {
+        std::process::exit;
+    }
 
 }
